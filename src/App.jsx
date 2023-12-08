376f47be86b18a3a85abf017cfc7535df3ce53ba
amrit <gillamrit29@gmail.com>
2023-12-08 23:41:22 +0530

    form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,57 @@
-import Child from "./Component/Child/Child";
+import { useState } from "react";
 
-const Parent = () => {
-  const getData = (data) => {
-    console.log("Inside PARENT = ", data);
+const App = () => {
+  const defaultValues = {
+    email: "",
+    password: "",
   };
-  let loggedInUser = "Amrit";
-  console.log("🚀 ~ file: App.jsx:8 ~ Parent ~ loggedInUser:", loggedInUser);
+  const [formFields, setFormFields] = useState(defaultValues);
+  const [isLoading, setIsLoading] = useState(false);
+  const { email, password } = formFields;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault(); // prevent page refresh
+    setIsLoading(true);
+    // api
+    setTimeout(() => {
+      console.log(formFields);
+      setFormFields(defaultValues);
+      setIsLoading(false);
+    }, 2000);
+  };
   return (
-    <>{loggedInUser ? <Child sendData={getData} /> : <p>Please Login</p>}</>
+    <form
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "90vh",
+      }}
+      onSubmit={handleSubmit}
+    >
+      <input
+        name="email"
+        value={email}
+        onChange={handleChange}
+        placeholder="email"
+      />
+      <br />
+      <input
+        name="password"
+        value={password}
+        onChange={handleChange}
+        placeholder="password"
+      />
+      <br />
+      <button disabled={isLoading}>
+        {isLoading ? "Loading...." : "Submit"}
+      </button>
+    </form>
   );
 };
 
-export default Parent;
-
-// const logData = (data) => {
-//   console.log(data);
-// };
-
-// const addFunc = (first, second, myFunc) => {
-//   const sum = first + second;
-//   myFunc(sum);
-// };
-
-// addFunc(1, 2, logData);
+export default App;
